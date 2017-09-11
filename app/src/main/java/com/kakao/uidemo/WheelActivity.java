621a1cb155cb3c0481f984d5b751af976891377c
jinyabo <jinyb>
2017-09-11 14:30:52 +0800

    1、添加wheel

---
+++ app/src/main/java/com/kakao/uidemo/WheelActivity.java
@@ -0,0 +1,66 @@
+package com.kakao.uidemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.kakao.uidemo.bean.Type;
+import com.kakao.uilib.wheel.CommonPickPopWinLoop;
+import com.kakao.uilib.wheel.utils.PickUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WheelActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn_common;
+    private Button btn_time;
+    private String chooseId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wheel);
+
+        btn_common = (Button) findViewById(R.id.btn_common);
+        btn_time = (Button) findViewById(R.id.btn_time);
+
+        btn_common.setOnClickListener(this);
+        btn_time.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btn_common) {
+            PickUtils.showCommonPicker(this, chooseId, getListTypes(), new CommonPickPopWinLoop.OnPickCompletedListener<Type>() {
+
+
+                @Override
+                public void onPickCompleted(String id, String name,Type type) {
+                    chooseId = id;
+                    btn_common.setText("类型选择：" + type.getText());
+                }
+            });
+
+        } else if (v == btn_time) {
+
+        }
+    }
+
+
+    public List<Type> getListTypes() {
+        List<Type> types = new ArrayList<>();
+        for (int i = 0; i < 15; i++) {
+            Type type = new Type();
+            type.setId((i + 1) + "");
+            type.setName("类型" + (i + 1));
+
+            types.add(type);
+        }
+
+        return types;
+    }
+}
